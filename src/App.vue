8b02e7752ce0b9dd863833c3016024d05db7a74b
fuji <fuji8@users.noreply.github.com>
2019-04-16 03:24:14 +0900

    予約検索の表示のみ

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,26 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        予約の確認
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <search />
     </v-content>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import search from './components/search'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    search
   },
   data () {
     return {
---
+++ src/components/search.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-layout>
+        <v-flex xs12 md6>
+          <v-text-field
+            v-model="traQID"
+            :counter="10"
+            label="traQID"
+            placeholder="fuji"
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs12 md6>
+          <v-text-field
+            v-model="dateBegin"
+            :counter="10"
+            label="begin"
+            placeholder="2018-08-10"
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs12 md6>
+          <v-text-field
+            v-model="dateEnd"
+            :counter="10"
+            label="end"
+            placeholder="2018-08-10"
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs12 md4>
+          <v-btn @click="submit">submit</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    traQID: '',
+    dateBegin: '',
+    dateEnd: ''
+  }),
+  methods: {
+    submit () {
+      console.log(this.traQID, this.dateBegin, this.dateEnd)
+    }
+  }
+}
+</script>
