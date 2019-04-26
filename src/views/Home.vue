e6def96f92b1d082abbe3fd03c0dfa22f96d2437
fuji <fuji8@users.noreply.github.com>
2019-04-26 23:06:47 +0900

    routerの使用準備

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,41 @@
 <template>
-  <HelloWorld />
+    <v-container grid-list-xl>
+      <v-layout>
+        <v-flex>
+          <search />
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap>
+        <v-flex lg9>
+          <h1>予約情報
+            <v-btn icon large>
+             <v-icon>add</v-icon>
+           </v-btn>
+          </h1>
+         <reservationTable />
+        </v-flex>
+        <v-flex lg3>
+          <h1>グループ情報
+            <v-btn icon large>
+             <v-icon>add</v-icon>
+           </v-btn>
+          </h1>
+         <groupTable />
+        </v-flex>
+      </v-layout>
+      </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import search from '../components/search'
+import reservationTable from '../components/reservationsTable'
+import groupTable from '../components/groupsTable'
 
-  export default {
-    components: {
-      HelloWorld
-    }
+export default {
+  components: {
+    search,
+    reservationTable,
+    groupTable
   }
+}
 </script>
