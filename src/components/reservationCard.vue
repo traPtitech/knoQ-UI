9a72ddb3d7d708be0eb2981e39296593c3730f61
fuji <fuji8@users.noreply.github.com>
2019-05-17 01:50:52 +0900

    carousel and card

---
+++ src/components/reservationCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-card color="blue-grey darken-2" class="title">
+    <v-card-title primary-title>
+      <v-container>
+        <v-layout>
+          <v-flex xs12>
+            <div class="headline">{{ reservation.name }}</div>
+          </v-flex>
+          <v-flex>
+            <div
+              class="headline text-md-right info--text"
+            >{{ reservation.date.getMonth() + 1 }}/{{ reservation.date.getDate() }}</div>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card-title>
+    <v-card-text>
+      <v-container fluid>
+        <v-layout>
+          <v-flex>
+            <div>グループ：{{ reservation.group.name }}</div>
+          </v-flex>
+          <v-flex>
+            <div class="text-xs-right">{{ reservation.group.members.length }}人</div>
+          </v-flex>
+        </v-layout>
+        <v-layout>
+          <v-flex>
+            <div>{{ reservation.room.place }}</div>
+          </v-flex>
+        </v-layout>
+        <v-layout>
+          <v-flex>
+            <div>
+              {{ reservation.time_start.slice(0,5) }} ~ {{ reservation.time_end.slice(0,5) }}
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn flat color="orange"></v-btn>
+      <v-btn flat color="orange"></v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['reservation']
+}
+</script>
--- src/components/reservationsTable.vue
+++ src/components/reservationsTable.vue
@@ -1,17 +1,36 @@
 <template>
-  <v-data-table
-    :headers="$store.state.revHeaders"
-    :items="$store.state.reservations"
-    class="elevation-1"
-  >
-    <template v-slot:items="props">
-      <td>{{ props.item.id }}</td>
-      <td class="text-xs-right">{{ props.item.group_id }}</td>
-      <td class="text-xs-right">{{ props.item.room_id }}</td>
-      <td class="text-xs-center">{{ props.item.date }}</td>
-      <td class="text-xs-right">{{ props.item.time_start }}</td>
-      <td class="text-xs-right">{{ props.item.time_end }}</td>
-      <td class="text-xs-right">{{ props.item.room.place }}</td>
-    </template>
-  </v-data-table>
+  <v-container>
+    <v-carousel interval="60000" height="1000">
+      <v-carousel-item :key="i" v-for="i in Math.floor(reservations.length / 4) + 1">
+        <v-container grid-list-md>
+          <v-layout row wrap align-center justify-space-around>
+            <v-flex xs12 sm6 :key="j" v-for="j in cards">
+              <reservationCard :reservation="reservations[(i-1)*4+j-1]"></reservationCard>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-carousel-item>
+    </v-carousel>
+  </v-container>
 </template>
+
+<script>
+import reservationCard from './reservationCard'
+export default {
+  components: {
+    reservationCard
+  },
+  data () {
+    return {}
+  },
+  methods: {},
+  computed: {
+    reservations () {
+      return this.$store.state.reservations
+    },
+    cards () {
+      return this.reservations.length % 4
+    }
+  }
+}
+</script>
