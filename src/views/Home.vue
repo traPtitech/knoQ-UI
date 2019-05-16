91965d3836bb195d77510013333e7a101a77151a
fuji <fuji8@users.noreply.github.com>
2019-05-17 01:50:31 +0900

    format

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,25 +2,27 @@
   <v-container grid-list-xl>
     <v-layout>
       <v-flex>
-        <search />
+        <search/>
       </v-flex>
     </v-layout>
     <v-layout row wrap>
       <v-flex>
-        <h1>予約情報
+        <h1>
+          予約情報
           <v-btn icon large @click="$router.push({ name: 'ReservationNew' })">
-           <v-icon>add</v-icon>
-         </v-btn>
+            <v-icon>add</v-icon>
+          </v-btn>
         </h1>
-       <reservationTable />
+        <reservationTable/>
       </v-flex>
       <v-flex>
-        <h1>グループ情報
+        <h1>
+          グループ情報
           <v-btn icon large @click="$router.push({ name: 'GroupNew' })">
-           <v-icon>add</v-icon>
-         </v-btn>
+            <v-icon>add</v-icon>
+          </v-btn>
         </h1>
-       <groupTable />
+        <groupTable/>
       </v-flex>
     </v-layout>
   </v-container>
