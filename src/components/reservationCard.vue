97f961b5f1a0229532b29bab7129956f7d3dc6aa
fuji <fuji8@users.noreply.github.com>
2019-05-19 22:00:02 +0900

    UI

--- src/components/reservationCard.vue
+++ src/components/reservationCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card color="blue-grey darken-2" class="title" height="350" min-width="220">
+  <v-card color="blue-grey darken-2" class="title white--text" height="350" min-width="220">
     <v-card-title primary-title>
       <v-container>
         <v-layout>
@@ -9,16 +9,16 @@
           <v-flex>
             <div
               class="headline text-xs-right info--text"
-            ><v-icon>calendar_today</v-icon> {{ reservation.date.getMonth() + 1 }}/{{ reservation.date.getDate() }}</div>
+            ><v-icon color="white">calendar_today</v-icon> {{ reservation.date.getMonth() + 1 }}/{{ reservation.date.getDate() }}</div>
           </v-flex>
         </v-layout>
       </v-container>
     </v-card-title>
     <v-card-text>
-      <v-container fluid>
+      <v-container fluid grid-list-xl>
         <v-layout>
           <v-flex>
-            <div><v-icon>people</v-icon> {{ reservation.group.name }}</div>
+            <div><v-icon color="white">people</v-icon> {{ reservation.group.name }}</div>
           </v-flex>
           <v-flex>
             <div class="text-xs-right">{{ reservation.group.members.length }}人</div>
@@ -26,13 +26,13 @@
         </v-layout>
         <v-layout>
           <v-flex>
-            <div><v-icon>place</v-icon> {{ reservation.room.place }}</div>
+            <div><v-icon color="white">place</v-icon> {{ reservation.room.place }}</div>
           </v-flex>
         </v-layout>
         <v-layout>
           <v-flex>
             <div>
-              <v-icon>watch_later</v-icon> {{ reservation.time_start.slice(0,5) }} ~ {{ reservation.time_end.slice(0,5) }}
+              <v-icon color="white">watch_later</v-icon> {{ reservation.time_start.slice(0,5) }} ~ {{ reservation.time_end.slice(0,5) }}
             </div>
           </v-flex>
         </v-layout>
