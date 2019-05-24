15891262958beec9360fe3e5adb9b103cb4cfa08
fuji <fuji8@users.noreply.github.com>
2019-05-25 00:48:06 +0900

    checkbox

--- src/views/GroupNew.vue
+++ src/views/GroupNew.vue
@@ -4,12 +4,13 @@
       <v-flex xs12>
         <h1>グループ追加</h1>
       </v-flex>
-            <v-flex xs5 mt-5>
+      <v-flex xs10 mt-5>
         <v-card>
           <v-card-text>
             <v-form>
-               <v-text-field v-model="group.name" label="名前"></v-text-field>
-               <v-text-field v-model="group.description" label="説明"></v-text-field>
+                <v-text-field v-model="group.name" label="名前"></v-text-field>
+                <v-text-field v-model="group.description" label="説明"></v-text-field>
+                <!--
                 <v-select
                   v-model="group.members"
                   :items="$store.state.allUsers"
@@ -22,6 +23,29 @@
                   persistent-hint
                   return-object
                 ></v-select>
+                -->
+                <v-container fluid>
+                  <p>{{ group.members }} {{ ID }}</p>
+                  <v-text-field
+                    v-model="ID"
+                    label="traQID"
+                  ></v-text-field>
+                  <v-layout row wrap>
+                    <v-flex xs4 v-for="member in targetMembers" :key="member.traq_id">
+                      <v-checkbox v-model="group.members" :value="member" >
+                        <template v-slot:label>
+                          <v-avatar size=32>
+                            <img
+                              :src="'https://q.trapti.tech/static/icon/' + member.traq_id + '/64.png'"
+                              :alt="member.traq_id"
+                            >
+                          </v-avatar>
+                          <span style="margin-left:10px;">{{ member.traq_id }}</span>
+                        </template>
+                      </v-checkbox>
+                    </v-flex>
+                  </v-layout>
+               </v-container>
                <v-btn @click="$router.push({ name: 'Home' })">キャンセル</v-btn>
                <v-btn color="info" @click="postGroup(group), $router.push({ name: 'Home' })">保存</v-btn>
             </v-form>
@@ -37,9 +61,11 @@ import { mapActions, mapGetters } from 'vuex'
 export default {
   data () {
     return {
+      ID: '',
       group: {
         members: [this.$store.state.loginUser]
-      }
+      },
+      targetMembers: this.$store.state.allUsers
     }
   },
   created: function () {
@@ -50,6 +76,20 @@ export default {
     ...mapGetters(['gettraQIDs']),
     save: function () {
       console.log(this.group)
+    }
+  },
+  watch: {
+    ID: function () {
+      if (this.ID !== '') {
+        this.targetMembers = []
+        for (let v of this.$store.state.allUsers) {
+          if (v.traq_id.indexOf(this.ID) === 0) {
+            this.targetMembers.push(v)
+          }
+        }
+      } else {
+        this.targetMembers = this.$store.state.allUsers
+      }
     }
   }
 }
