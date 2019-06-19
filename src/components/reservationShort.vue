f57861c4b8876baa7649f7cbaf2074ebc4488f22
fuji <fuji8@users.noreply.github.com>
2019-06-19 16:35:53 +0900

    予約　ショート

---
+++ src/components/reservationShort.vue
@@ -0,0 +1,32 @@
+<template>
+<v-card>
+  <v-card-text>
+    {{reservation.date}}
+  </v-card-text>
+  <v-card-text>
+    <v-layout row wrap>
+      <v-flex class="font-weight-bold">
+        {{reservation.name}}
+      </v-flex>
+      <v-flex class="green--text">
+        <li>{{reservation.group.name}}</li>
+      </v-flex>
+      <v-flex>
+        <v-icon small>place</v-icon>{{reservation.place}}
+      </v-flex>
+      <v-flex text-xs-center>
+        {{reservation.date}}
+      </v-flex>
+      <v-flex text-xs-right>
+        {{reservation.timeStart}} - {{reservation.timeEnd}}
+      </v-flex>
+    </v-layout>
+  </v-card-text>
+</v-card>
+</template>
+
+<script>
+export default {
+  props: ['reservation']
+}
+</script>
