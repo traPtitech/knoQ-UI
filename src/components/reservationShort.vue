b1dfa9430a84d4087e33c9b19fbc991b1566e759
fuji <fuji8@users.noreply.github.com>
2019-06-24 22:57:06 +0900

    iPhone SE

--- src/components/reservationShort.vue
+++ src/components/reservationShort.vue
@@ -1,6 +1,6 @@
 <template>
 <v-card>
-  <v-card-text>
+  <v-card-text  style="font-size: 100%;">
     <v-layout row wrap>
       <v-flex hidden-xs-only sm4 class="font-weight-bold" overflow-hidden>
         <span style="cursor: pointer"
@@ -16,7 +16,7 @@
           ◦{{reservation.group.name}}
         </span>
       </v-flex>
-      <v-flex hidden-sm-and-up xs6>
+      <v-flex hidden-sm-and-up xs5>
           <div style="cursor: pointer;" class="font-weight-bold"
             @click="$router.push({ name: 'ReservationDescription', params: { id: reservation.id } })"
           >
@@ -41,7 +41,7 @@
         {{reservation.time_start.slice(0,5)}} -{{reservation.time_end.slice(0,5)}}
       </v-flex>
       <v-flex hidden-sm-and-up xs3.5 text-xs-right>
-        <div >
+        <div>
           {{date}}
         </div>
         <span style="font-size: 75%;" class="font-weight-bold">{{reservation.time_start.slice(0,5)}} -</span>
