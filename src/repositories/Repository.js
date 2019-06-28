c1440d0db4d10f391057a5d0d565ae52729e8893
fuji <fuji8@users.noreply.github.com>
2019-06-28 12:57:03 +0900

    /api

--- src/repositories/Repository.js
+++ src/repositories/Repository.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3000/api'
+let baseURL = '/api'
+
+// buildするとproductionが入る
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3000/api'
+}
 
 export default axios.create({
   baseURL
