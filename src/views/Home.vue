c86207352f882acd325c3f1553db213def98248e
fuji <fuji8@users.noreply.github.com>
2019-07-01 16:14:26 +0900

    ホーム画面に今日の進捗部屋が表示されるようにした

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,12 @@
   <v-container fluid grid-list-xl>
     <v-layout column>
       <v-flex>
+        <h1>今日の進捗部屋</h1>
+        <roomsExpansion :rooms=todayRooms></roomsExpansion>
+      </v-flex>
+      <v-flex>
         <h1>
-          予約情報
+          あなたの予約
           <v-btn icon large @click="$router.push({ name: 'ReservationNew' })">
             <v-icon>add</v-icon>
           </v-btn>
@@ -12,7 +16,7 @@
       </v-flex>
       <v-flex>
         <h1>
-          グループ情報
+          あなたのグループ
           <v-btn icon large @click="$router.push({ name: 'GroupNew' })">
             <v-icon>add</v-icon>
           </v-btn>
@@ -24,21 +28,39 @@
 </template>
 
 <script>
-import search from '../components/search'
+import roomsExpansion from '../components/roomsExpansion'
 import reservationsCarousel from '../components/reservationsCarousel'
 import reservationCardShorts from '../components/reservationCardShorts'
 import groupsTable from '../components/groupsTable'
+import { RepositoryFactory } from '../repositories/RepositoryFactory'
 import { mapActions } from 'vuex'
+import moment from 'moment'
+const RoomsRepository = RepositoryFactory.set('rooms')
 
 export default {
   components: {
-    search,
+    roomsExpansion,
     reservationsCarousel,
     groupsTable,
     reservationCardShorts
   },
+  data () {
+    return {
+      todayRooms: []
+    }
+  },
   created: async function () {
     await this.getUserMe()
+    const today = moment(new Date()).format('YYYY-MM-DD')
+
+    const { data } = await RoomsRepository.get(
+      {
+        dateBegin: today,
+        dateEnd: today
+      }
+    )
+    this.todayRooms = data
+    console.log(data)
     this.getMyGroups()
     this.getMyReservations()
   },
