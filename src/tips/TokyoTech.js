21999665b28adc2293eb623656d97bc25e9d4fc7
you10_19 <you10rinjp.youto@gmail.com>
2019-07-25 15:11:16 +0900

    部屋名かリンクを返す関数を追加

---
+++ src/tips/TokyoTech.js
@@ -0,0 +1,21 @@
+export default {
+    searchRoom(room){
+        const initial = room.charAt();
+        let building;
+        switch (initial){
+            case 'S': building = 'minami';
+            break;
+            case 'W': building = 'nisi';
+            break;
+            case 'H': building = 'honkan';
+            break;
+            case 'M': building = 'midori';
+            break;
+            case 'I': building = 'isikawa';
+            break;
+            default: console.log('error');
+        }
+        const link = 'https://www.titech.ac.jp/enrolled/facilities/rooms/pdf/picture/' + building + '/' + room + '.pdf';
+        return link
+    }
+}
