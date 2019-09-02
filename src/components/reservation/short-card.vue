8f374754196831e44e1f2edbb9e86ef581ba2d46
fuji <fuji8@users.noreply.github.com>
2019-09-03 02:31:49 +0900

    UI fix

--- src/components/reservation/short-card.vue
+++ src/components/reservation/short-card.vue
@@ -6,85 +6,66 @@
         wrap
       >
         <v-flex 
-          hidden-xs-only 
-          sm4 
+          xs5 
+          sm7
           class="font-weight-bold" 
-          overflow-hidden
         >
-          <span 
-            style="cursor: pointer"
-            @click="$router.push({ name: 'ReservationDescription', params: { id: reservation.id } })"
+          <v-layout
+            wrap
           >
-            {{ reservation.name }}
-          </span>
+            <v-flex
+              xs12
+              sm7
+              style="cursor: pointer"
+              @click="$router.push({ name: 'ReservationDescription', params: { id: reservation.id } })"
+            >
+              {{ reservation.name }}
+            </v-flex>
+            <v-flex
+              :class="groupColor"
+              xs12
+              sm5
+            >
+              <span 
+                style="cursor: pointer"
+                @click="$router.push({ name: 'GroupDescription', params: { id: reservation.group_id } })"
+              >
+                ◦{{ reservation.group.name }}
+              </span>
+            </v-flex>
+          </v-layout>
         </v-flex>
-        <v-flex 
-          :class="groupColor" 
-          hidden-xs-only 
-          sm3
+        <v-flex
+          xs7
+          sm5
         >
-          <span 
-            style="cursor: pointer"
-            @click="$router.push({ name: 'GroupDescription', params: { id: reservation.group_id } })"
+          <v-layout
+            wrap
+            text-xs-left
           >
-            ◦{{ reservation.group.name }}
-          </span>
-        </v-flex>
-        <v-flex 
-          hidden-sm-and-up 
-          xs5
-        >
-          <div 
-            style="cursor: pointer;" 
-            class="font-weight-bold"
-            @click="$router.push({ name: 'ReservationDescription', params: { id: reservation.id } })"
-          >
-            {{ reservation.name }}
-          </div>
-          <span 
-            :class="groupColor"
-            style="cursor: pointer;
-            padding-left: 7px;
-            font-size: 75%;"
-            @click="$router.push({ name: 'GroupDescription', params: { id: reservation.group_id } })"
-          >
-            ◦{{ reservation.group.name }}
-          </span>
-        </v-flex>
-        <v-flex 
-          sm1 
-          text-xs-right
-          style="cursor: pointer;" 
-          @click="openClassinfo(reservation.room.place)"
-        >
-          <v-icon small>place</v-icon>{{ reservation.room.place }}
-        </v-flex>
-        <v-flex 
-          hidden-xs-only 
-          sm2 
-          text-xs-right
-        >
-          {{ date }}
-        </v-flex>
-        <v-flex 
-          hidden-xs-only 
-          sm2 
-          text-xs-right
-        >
-          {{ reservation.time_start.slice(0,5) }} -{{ reservation.time_end.slice(0,5) }}
-        </v-flex>
-        <v-flex 
-          hidden-sm-and-up 
-          xs3 
-          text-xs-right
-        >
-          <div>
-            {{ date.slice(0,5) }}
-          </div>
-          <span 
-            style="font-size: 75%;" 
-            class="font-weight-bold"
-          >{{ reservation.time_start.slice(0,5) }} -</span>
+            <v-flex 
+              xs6
+              sm4
+              style="cursor: pointer;" 
+              @click="openClassinfo(reservation.room.place)"
+            >
+              <v-icon small>place</v-icon>{{ reservation.room.place }}
+            </v-flex>
+
+            <v-flex 
+              xs6
+              sm4
+            >
+              {{ date }}
+            </v-flex>
+            <v-flex 
+              xs12
+              sm4
+              class="font-weight-bold"
+            >
+              {{ reservation.time_start.slice(0,5) }} - {{ reservation.time_end.slice(0,5) }}
+            </v-flex>
+          </v-layout>
         </v-flex>
       </v-layout>
     </v-card-text>
