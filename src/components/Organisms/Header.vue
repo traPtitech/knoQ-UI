d57a26d4c9cc37153fe10899008133c017bcdc44
wasabi315 <wasabi.nvim.st@gmail.com>
2019-11-08 02:18:18 +0900

    Add: Header sans search field

---
+++ src/components/Organisms/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app-bar app>
+    <v-toolbar-title
+      class="headline font-weight-bold"
+      style="cursor: pointer"
+      @click="$router.push({ name: 'Home' })"
+    >
+      Room
+    </v-toolbar-title>
+    <v-spacer />
+    <Avatar :size="32" :traq-id="'wasabi'" />
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import Avatar from '@/components/Molecules/Avatar.vue'
+
+@Component({
+  components: {
+    Avatar,
+  },
+})
+export default class Header extends Vue {}
+</script>
