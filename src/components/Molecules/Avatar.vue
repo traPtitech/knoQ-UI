0b6d30e639b2a1e4879cafb92f77b1caf3f04eeb
wasabi315 <wasabi.nvim.st@gmail.com>
2019-11-08 02:17:09 +0900

    Add Avatar component

---
+++ src/components/Molecules/Avatar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-avatar :size="size">
+    <img
+      :src="`https://q.trapti.tech/static/icon/${traqId}/64.png`"
+      alt="avatar"
+    />
+  </v-avatar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Avatar extends Vue {
+  @Prop()
+  size!: number
+
+  @Prop()
+  traqId!: string
+}
+</script>
