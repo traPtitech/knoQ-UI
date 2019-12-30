1efb70a9f6a5c7ea592478c0d4f3d36fb95e3705
wasabi315 <wasabi.nvim.st@gmail.com>
2019-12-30 15:13:23 +0900

    Add help

--- src/components/Main/Sidebar/Footer.vue
+++ src/components/Main/Sidebar/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row justify="space-around">
     <span>Room {{ version }}</span>
-    <v-icon>mdi-help</v-icon>
+    <v-icon @click="openHelpPage">mdi-help</v-icon>
   </v-row>
 </template>
 
@@ -12,5 +12,11 @@ import { Component } from 'vue-property-decorator'
 @Component
 export default class Footer extends Vue {
   version = 'ver0.0.1'
+
+  openHelpPage() {
+    window.open(helpPageURL)
+  }
 }
+
+const helpPageURL = 'https://md.trapti.tech/0spW8adnSyCUaV_wioVlWA'
 </script>
