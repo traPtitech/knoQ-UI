8dcf167b05a72105892a9409e9fa27011f4183b9
wasabi315 <wasabi.nvim.st@gmail.com>
2019-12-31 10:40:23 +0900

    Add Layout component

---
+++ src/components/shared/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container fluid class="mx-4 py-2 py-sm-7">
+    <v-row justify="center">
+      <v-col sm="9" cols="12">
+        <slot />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class Layout extends Vue {}
+</script>
