234d2642772a7fe3623de47da4a33f104dbd7094
wasabi315 <wasabi.nvim.st@gmail.com>
2020-01-02 16:57:40 +0900

    Add WIP page component

---
+++ src/pages/WIP.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout>
+    <p class="text-center display-1">
+      Sorry, now work in progress...
+    </p>
+    <p class="text-center">
+      <v-icon size="10rem">mdi-worker</v-icon>
+    </p>
+  </Layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import Layout from '@/components/shared/Layout.vue'
+
+@Component({
+  components: {
+    Layout,
+  },
+})
+export default class WIP extends Vue {}
+</script>
