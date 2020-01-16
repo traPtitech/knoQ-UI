2bb91088eacb87c0886df7114118c610f1a3fd19
wasabi315 <wasabi.nvim.st@gmail.com>
2020-01-16 17:27:41 +0900

    Remove unneeded code

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
   <v-app>
-    <v-snackbar v-model="snackbar" top>
-      {{ snackMessage }}
-      <v-btn color="pink" flat @click="snackbar = false">
-        Close
-      </v-btn>
-    </v-snackbar>
     <Sidebar ref="sidebar" />
     <NavBar @toggleDrawer="toggleDrawer" />
     <v-content>
@@ -14,74 +8,22 @@
   </v-app>
 </template>
 
-<script>
-import Sidebar from '@/components/Main/Sidebar'
-import NavBar from '@/components/Main/NavBar'
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import Sidebar from '@/components/Main/Sidebar.vue'
+import NavBar from '@/components/Main/NavBar.vue'
 import { RepositoryFactory } from './repositories/RepositoryFactory'
-import { mapActions } from 'vuex'
 
-const RoomsRepository = RepositoryFactory.get('rooms')
-export default {
-  name: 'App',
+@Component({
   components: {
     Sidebar,
     NavBar,
   },
-  data() {
-    return {
-      drawer: false,
-      query: '',
-      dialog: false,
-      snackbar: false,
-      snackMessage: '',
-    }
-  },
-  computed: {
-    traQID() {
-      if (!this.$store.state.loginUser.traq_id) {
-        return 'fuji'
-      }
-      return this.$store.state.loginUser.traq_id
-    },
-  },
-  created: async function() {
-    await this.getUserMe()
-  },
-  methods: {
-    toggleDrawer: function() {
-      this.$refs.sidebar.toggleDrawer()
-    },
-    roomsAll: async function() {
-      try {
-        const response = await RoomsRepository.postAll()
-        console.log(response)
-        this.snackMessage = response.statusText
-        this.snackbar = true
-      } catch (error) {
-        this.snackMessage = error
-        this.snackbar = true
-      }
-    },
-    trigger: function(event) {
-      if (event.keyCode === 13) {
-        this.search()
-      }
-    },
-    search: function() {
-      if (this.query !== '') {
-        this.dialog = false
-        this.$router.push({ name: 'Search', query: { q: this.query } })
-      }
-    },
-    ...mapActions(['getUserMe']),
-  },
+})
+export default class App extends Vue {
+  toggleDrawer() {
+    ;(this.$refs.sidebar as Sidebar).toggleDrawer()
+  }
 }
 </script>
-
-<style>
-.v-dialog {
-  position: absolute;
-  top: 0;
-  padding-right: 50px;
-}
-</style>
