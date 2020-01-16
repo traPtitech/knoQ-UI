1d1dee2be64b3948f2a1c5f157f1ed25616e8043
wasabi315 <wasabi.nvim.st@gmail.com>
2020-01-16 18:08:42 +0900

    Change file name

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import vuetify from './plugins/vuetify'
 import App from './App.vue'
-import router from '@/router/router'
-import store from '@/store/store'
+import router from '@/router'
+import store from '@/store'
 
 Vue.config.productionTip = false
 
