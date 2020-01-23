2db237f3e225c01e6001e8e93f9ce68201270641
wasabi315 <wasabi.nvim.st@gmail.com>
2020-01-23 19:43:39 +0900

    Add type declarations

---
+++ src/types/api.d.ts
@@ -0,0 +1,236 @@
+declare namespace API {
+  namespace Rooms {
+    namespace Get {
+      export interface Params {
+        dateBegin?: string
+        dateEnd?: string
+      }
+      namespace Responses {
+        export type $200 = Schemas.Room[]
+      }
+    }
+
+    namespace Post {
+      export interface RequestBody {
+        place: string
+        date: string
+        timeStart: string
+        timeEnd: string
+      }
+      namespace Responses {
+        export type $201 = Schemas.Room
+      }
+    }
+
+    namespace $RoomId {
+      namespace Get {
+        namespace Responses {
+          export type $200 = Schemas.Room
+        }
+      }
+    }
+
+    namespace Private {
+      namespace Get {
+        export interface RequestBody {
+          place: string
+          date: string
+          timeStart: string
+          timeEnd: string
+        }
+        namespace Responses {
+          export type $201 = Schemas.Room
+        }
+      }
+    }
+
+    namespace All {
+      namespace Post {
+        namespace Responses {
+          export type $201 = Schemas.Room[]
+        }
+      }
+    }
+  }
+
+  namespace Events {
+    namespace Get {
+      export interface Params {
+        name: string
+        groupId: string
+        roomId: string
+        dateBegin: string
+        dateEnd: string
+      }
+      namespace Responses {
+        export type $200 = Schemas.Event[]
+      }
+    }
+
+    namespace Post {
+      export interface RequestBody {
+        name: string
+        description: string
+        sharedRoom: boolean
+        timeStart: string
+        timeEnd: string
+        imageId: boolean
+        roomId: boolean
+        groupId: boolean
+        tags: {
+          name: string
+        }[]
+      }
+      namespace Responses {
+        export type $201 = Schemas.Event
+      }
+    }
+
+    namespace $EventId {
+      namespace Get {
+        namespace Responses {
+          export type $200 = Schemas.Event
+        }
+      }
+
+      namespace Put {
+        export interface RequestBody {
+          name: string
+          description: string
+          sharedRoom: boolean
+          timeStart: string
+          timeEnd: string
+          imageId: boolean
+          roomId: boolean
+          groupId: boolean
+          tags: {
+            name: string
+            locked: boolean
+          }[]
+        }
+        namespace Responses {
+          export type $200 = Schemas.Event
+        }
+      }
+
+      namespace Favorite {
+        namespace Put {
+          namespace Responses {
+            export type $200 = Schemas.Event
+          }
+        }
+      }
+
+      namespace Tags {
+        namespace Post {
+          export interface RequestBody {
+            id: string
+          }
+          namespace Responses {
+            export type $200 = Schemas.Event
+          }
+        }
+      }
+    }
+  }
+
+  namespace Groups {
+    namespace Get {
+      namespace Responses {
+        export type $200 = Schemas.Group[]
+      }
+    }
+
+    namespace Post {
+      export interface RequestBody {
+        name: string
+        description: string
+        imageId: string
+        open: string
+        member: string[]
+      }
+      namespace Responses {
+        export type $201 = Schemas.Group
+      }
+    }
+
+    namespace $GroupId {
+      namespace Get {
+        namespace Responses {
+          export type $200 = Schemas.Group
+        }
+      }
+
+      namespace Put {
+        export interface RequestBody {
+          name: string
+          description: string
+          imageId: string
+          open: string
+          member: string[]
+        }
+        namespace Responses {
+          export type $200 = Schemas.Group
+        }
+      }
+
+      namespace Members {
+        namespace Me {
+          namespace Put {
+            namespace Responses {
+              export type $200 = Schemas.Group
+            }
+          }
+        }
+      }
+    }
+  }
+
+  namespace Users {
+    namespace Get {
+      namespace Responses {
+        export type $200 = Schemas.User
+      }
+    }
+    namespace Me {
+      namespace Get {
+        namespace Responses {
+          export type $200 = Schemas.Me
+        }
+      }
+      namespace Events {
+        namespace Get {
+          namespace Responses {
+            export type $200 = Schemas.Event[]
+          }
+        }
+      }
+
+      namespace Groups {
+        namespace Get {
+          namespace Responses {
+            export type $200 = string[]
+          }
+        }
+      }
+    }
+  }
+
+  namespace Tags {
+    namespace Get {
+      namespace Responses {
+        export type $200 = Schemas.Tag[]
+      }
+    }
+
+    namespace Post {
+      export interface RequestBody {
+        name: string
+      }
+
+      namespace Response {
+        export type $200 = Schemas.Tag
+      }
+    }
+  }
+}
--- src/types/schemas.d.ts
+++ src/types/schemas.d.ts
@@ -1,84 +1,69 @@
 declare namespace Schemas {
-  // type aliases
-  export type Time = string
-  export type Date = string
-  export type DateTime = string
-
-  export interface RoomDetail {
-    id: number
+  export interface Room {
+    roomId: string
     place: string
-    date: Date
-    time_start: Time
-    time_end: Time
-    created_at: DateTime
-    updated_at: DateTime
-    tags: (Tag & { locked: boolean })[]
-    available_time: {
-      time_start: Time
-      time_end: Time
+    date: string
+    timeStart: string
+    timeEnd: string
+    public: boolean
+    availableTime: {
+      timeStart: string
+      timeEnd: string
     }[]
+    createdBy: string
+    createdAt: string
+    updatedAt: string
   }
 
   export interface Event {
+    eventId: string
     name: string
     description: string
-    allow_together: string
-    time_start: Time
-    time_end: Time
-    tags: (Tag['name'] & { locked: boolean })[]
-    room_id: number
-    group_id: number
-  }
-
-  export interface EventDetail {
-    id: number
-    name: string
-    description: string
-    allow_together: string
-    time_start: Time
-    time_end: Time
-    tags: (Tag & { locked: boolean })[]
-    group: GroupDetail
-    room: RoomDetail
-    created_by: User['traq_id']
-    created_at: DateTime
-    updated_at: DateTime
+    private: boolean
+    timeStart: string
+    timeEnd: string
+    imageId: string
+    roomId: string
+    groupId: string
+    favorite: boolean
+    tags: {
+      name: string
+      locked: boolean
+    }[]
+    createdBy: string
+    createdAt: string
+    updatedAt: string
   }
 
   export interface Group {
+    groupId: string
     name: string
     description: string
-    join_freely: boolean
-    members: { traq_id: User['traq_id'] }[]
-    tags: (Tag['name'] & { locked: boolean })[]
-  }
-
-  export interface GroupDetail {
-    id: number
-    name: string
-    description: string
-    join_freely: string
-    members: User[]
-    tags: (Tag & { locked: boolean })[]
-    created_by: User['traq_id']
-    created_at: DateTime
-    updated_at: DateTime
+    imageId: string
+    open: string
+    members: string[]
+    isTraQGroup: boolean
+    createdBy: string
+    createdAt: string
+    updatedAt: string
   }
 
   export interface Tag {
-    id: number
     name: string
-    official: boolean
-    for_room: boolean
-    for_group: boolean
-    for_event: boolean
+    official: true
   }
 
   export interface User {
-    traq_id: string
+    userId: string
+    name: string
   }
 
-  export interface Me extends User {
-    admin: boolean
+  export interface Me {
+    userId: string
+    admin: string
+  }
+
+  export interface Image {
+    imageId: string
   }
 }
