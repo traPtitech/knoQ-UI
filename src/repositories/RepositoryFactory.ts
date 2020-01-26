3c57e6fc6f359e0db5482d7fe515a137f13770aa
wasabi315 <wasabi.nvim.st@gmail.com>
2020-01-26 22:55:53 +0900

    Add repositories

--- src/repositories/RepositoryFactory.ts
+++ src/repositories/RepositoryFactory.ts
@@ -1,11 +1,15 @@
-import UsersRepository from './usersRepository'
-import GroupsRepository from './groupsRepository'
-import RoomsRepository from './roomsRepository'
+import EventsRepository from '@/repositories/eventsRepository'
+import GroupsRepository from '@/repositories/groupsRepository'
+import RoomsRepository from '@/repositories/roomsRepository'
+import TagsRepository from '@/repositories/tagsRepository'
+import UsersRepository from '@/repositories/usersRepository'
 
 const repositories = {
-  users: UsersRepository,
+  events: EventsRepository,
   groups: GroupsRepository,
   rooms: RoomsRepository,
+  users: UsersRepository,
+  tags: TagsRepository,
 }
 
 type Repositories = typeof repositories
