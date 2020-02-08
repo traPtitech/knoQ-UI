8beb26c3df4a1bb52ec04c49853f1af49e283f3e
wasabi315 <wasabi.nvim.st@gmail.com>
2020-02-08 11:09:50 +0900

    Change NavLink template

--- src/components/main/Sidebar/NavLink.vue
+++ src/components/main/Sidebar/NavLink.vue
@@ -1,11 +1,11 @@
 <template>
-  <v-list-item link>
-    <v-btn depressed rounded block @click="onClick()">
-      <v-icon>{{ icon }}</v-icon>
-      <v-list-item-title>
-        {{ title }}
-      </v-list-item-title>
-    </v-btn>
+  <v-list-item link :to="link" color="teal">
+    <v-list-item-icon>
+      <v-icon> {{ icon }} </v-icon>
+    </v-list-item-icon>
+    <v-list-item-content>
+      <v-list-item-title> {{ title }} </v-list-item-title>
+    </v-list-item-content>
   </v-list-item>
 </template>
 
@@ -15,17 +15,8 @@ import { Component, Prop } from 'vue-property-decorator'
 
 @Component
 export default class NavLink extends Vue {
-  @Prop()
-  title!: string
-
-  @Prop()
-  link!: string
-
-  @Prop()
-  icon!: string
-
-  onClick() {
-    this.$router.push(this.link)
-  }
+  @Prop() title!: string
+  @Prop() link!: string
+  @Prop() icon!: string
 }
 </script>
