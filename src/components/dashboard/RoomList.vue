70da331d5312279c615327198d05fc1d6c58f341
wasabi315 <wasabi.nvim.st@gmail.com>
2020-03-10 16:05:41 +0900

    Add components: RoomList*

---
+++ src/components/dashboard/RoomList.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row>
+    <v-col v-for="(room, i) in rooms" :key="i" sm="4" cols="12">
+      <RoomListItem v-bind="room" />
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import RoomListItem from '@/components/dashboard/RoomListItem.vue'
+
+@Component({
+  components: {
+    RoomListItem,
+  },
+})
+export default class RoomList extends Vue {
+  rooms = [
+    {
+      place: 'S512',
+      timeStart: '9:00 am',
+      timeEnd: '8:00 pm',
+    },
+    {
+      place: 'S514',
+      timeStart: '4:00 pm',
+      timeEnd: '8:00 pm',
+    },
+    {
+      place: 'H101',
+      timeStart: '5:00 pm',
+      timeEnd: '8:00 pm',
+    },
+  ]
+}
+</script>
