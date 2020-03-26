f36a210f3b54c3fa496b0da0217f100ad5d8e59b
wasabi315 <wasabi.nvim.st@gmail.com>
2020-03-26 10:27:31 +0900

    Event detail layout

--- src/pages/EventDetail.vue
+++ src/pages/EventDetail.vue
@@ -1,55 +1,80 @@
 <template>
   <v-container>
-    <v-row align="center" justify-sm="space-between">
-      <v-col class="mx-n1" sm="7" cols="12">
-        <h1 class="mb-3 display-1 font-weight-bold">{{ title }}</h1>
-        <EventTag v-for="(tag, i) in tags" :key="i" :name="tag" />
-      </v-col>
-      <v-col sm="3" cols="12">
-        <v-btn>
-          Join
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-img max-height="150" contain class="my-9" :src="img" />
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" sm="2">
-        Date
-      </v-col>
-      <v-col cols="12" sm="10">
-        {{ formattedSpan }}
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" sm="2">
-        Place
-      </v-col>
-      <v-col cols="12" sm="10">
-        {{ place }}
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" sm="2">
-        Group
-      </v-col>
-      <v-col cols="12" sm="10">
-        {{ group }}
-      </v-col>
-    </v-row>
-    <MarkdownField class="mt-10" :src="description" />
+    <v-card class="pa-9">
+      <div class="mb-12">
+        <h1 class="mb-1 display-1">{{ event.name }}</h1>
+        <div class="mb-3">
+          <template v-if="!isTagEditting">
+            <EventTag
+              v-for="(tag, i) in eventTags"
+              :key="i"
+              :name="tag"
+              class="mr-3"
+            />
+            <v-btn x-small icon title="Edit tags" @click="isTagEditting = true">
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+          </template>
+          <v-combobox
+            v-else
+            v-model="eventTags"
+            :items="tags"
+            autofocus
+            dense
+            chips
+            clearable
+            multiple
+            solo
+            @blur="onTagEditEnd"
+          >
+            <template #selection="{ item }">
+              <EventTag close :name="item" @click:close="removeTag(item)" />
+            </template>
+          </v-combobox>
+        </div>
+        <div class="text--secondary">
+          <span class="mr-3">by {{ group.name }}</span>
+          <v-btn
+            x-small
+            outlined
+            color="primary"
+            :to="`/groups/${event.groupId}`"
+          >
+            Learn more
+          </v-btn>
+        </div>
+      </div>
+      <div class="mb-5">
+        <div class="text--secondary mb-n1">Date</div>
+        <div class="headline">
+          {{ formattedTimeStart }}
+          <v-icon>mdi-chevron-right</v-icon>
+          {{ formattedTimeEnd }}
+        </div>
+      </div>
+      <div class="mb-5">
+        <div class="text--secondary mb-n1">Place</div>
+        <div class="headline">{{ room.place }}</div>
+        <div class="text--secondary body-2">
+          <v-icon :color="sharedRoomIcon.color">
+            {{ sharedRoomIcon.icon }}
+          </v-icon>
+          {{ sharedRoomString }}
+        </div>
+      </div>
+      <div class="mb-5">
+        <MarkdownField class="mt-10" :src="event.description" />
+      </div>
+    </v-card>
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { Component, Prop } from 'vue-property-decorator'
-import moment from 'moment'
 import EventTag from '@/components/shared/EventTag.vue'
 import MarkdownField from '@/components/shared/MarkdownField.vue'
+import { momentify } from '@/utils/date'
 
 @Component({
   components: {
@@ -58,35 +83,72 @@ import MarkdownField from '@/components/shared/MarkdownField.vue'
   },
 })
 export default class EventDetail extends Vue {
-  title = 'Vueもくもく会'
+  isTagEditting = false
+  tags = ['react', 'vue', 'ts', 'frontend', 'angular', 'haskell', 'monad']
 
-  tags = ['vue', 'ts', 'frontend']
+  event = {
+    eventId: 1,
+    roomId: 1,
+    groupId: 2,
+    name: 'Vueもくもく会',
+    tags: ['vue', 'ts', 'frontend'],
+    description: [
+      '## Vueもくもく会　開催決定!',
+      '自分だけの最強のVueコンポーネントをつくろう!!',
+      '- Vue初心者',
+      '- Vue強者',
+      '',
+      '参加をお待ちしてます!!',
+    ].join('\n'),
+    timeStart: '17:00',
+    timeEnd: '20:00',
+    sharedRoom: true,
+  }
 
-  img =
-    'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'
+  get eventTags(): string[] {
+    return this.event.tags
+  }
+  set eventTags(value: string[]) {
+    this.event.tags = value
+  }
 
-  description = [
-    '## Vueもくもく会　開催決定!',
-    '自分だけの最強のVueコンポーネントをつくろう!!',
-    '- Vue初心者',
-    '- Vue強者',
-    '',
-    '参加をお待ちしてます!!',
-  ].join('\n')
+  room = {
+    place: 'W933',
+    date: '2020-01-01',
+  }
 
-  date = '2020-01-01'
+  group = {
+    name: 'SysAd',
+  }
 
-  time_start = '17:00'
-  time_end = '20:00'
+  removeTag(name: string) {
+    const index = this.event.tags.indexOf(name)
+    if (index >= 0) this.event.tags.splice(index, 1)
+  }
 
-  place = 'W933'
+  get formattedTimeStart(): string {
+    return momentify(this.room.date, this.event.timeStart).format(
+      'MMM D, h:mma'
+    )
+  }
 
-  group = 'SysAd'
+  get formattedTimeEnd(): string {
+    return momentify(this.room.date, this.event.timeEnd).format('MMM D, h:mma')
+  }
 
-  get formattedSpan(): string {
-    const start = moment(`${this.date} ${this.time_start}`).format('lll')
-    const end = moment(`${this.date} ${this.time_end}`).format('lll')
-    return `${start} ~ ${end}`
+  get sharedRoomString(): string {
+    return this.event.sharedRoom ? '部屋の共用可能' : '部屋の共用不可能'
+  }
+
+  get sharedRoomIcon() {
+    return this.event.sharedRoom
+      ? { icon: 'mdi-door-open', color: 'success' }
+      : { icon: 'mdi-door-closed-lock', color: 'error' }
+  }
+
+  onTagEditEnd() {
+    this.isTagEditting = false
+    alert(this.event.tags)
   }
 }
 </script>
