d1bb0eecd5ce4e17f879fc6d96b8902659627514
wasabi315 <wasabi.nvim.st@gmail.com>
2020-03-31 18:14:34 +0900

    Complete: event edit form

--- src/pages/EventEdit.vue
+++ src/pages/EventEdit.vue
@@ -1,84 +1,88 @@
 <template>
   <v-container>
-    <v-stepper v-model="step" class="mb-5">
-      <v-stepper-header>
-        <v-stepper-step :complete="step > 1" step="1">
-          イベント内容
-        </v-stepper-step>
-        <v-divider />
-        <v-stepper-step :complete="step > 2" step="2">
-          日時・場所
-        </v-stepper-step>
-        <v-divider />
-        <v-stepper-step :complete="step > 3" step="3">
-          確認
-        </v-stepper-step>
-      </v-stepper-header>
+    <ProgressCircular v-if="status === 'loading'" />
+    <LoadFailedText v-else-if="status === 'error'" />
+    <template v-else>
+      <v-stepper v-model="step" class="mb-5">
+        <v-stepper-header>
+          <v-stepper-step :complete="step > 1" step="1">
+            イベント内容
+          </v-stepper-step>
+          <v-divider />
+          <v-stepper-step :complete="step > 2" step="2">
+            日時・場所
+          </v-stepper-step>
+          <v-divider />
+          <v-stepper-step :complete="step > 3" step="3">
+            確認
+          </v-stepper-step>
+        </v-stepper-header>
 
-      <v-stepper-items class="pb-1">
-        <v-stepper-content step="1">
-          <EventFormContent v-model="valid1" v-bind.sync="content" />
-          <FormNextButton :disabled="!valid1" @click="step = 2">
-            Continue
-          </FormNextButton>
-        </v-stepper-content>
-        <v-stepper-content step="2">
-          <v-checkbox
-            v-model="isPrivate"
-            label="traPが予約していない場所で開催する"
-          />
-          <EventFormReservationPublic
-            v-show="!isPrivate"
-            v-model="validPublic"
-            v-bind.sync="reservationPublic"
-          />
-          <EventFormReservationPrivate
-            v-show="isPrivate"
-            v-model="validPrivate"
-            v-bind.sync="reservationPrivate"
-          />
-          <FormBackButton class="mr-2" @click="step = 1">
-            Back
-          </FormBackButton>
-          <FormNextButton :disabled="!valid2" @click="step = 3">
-            Continue
-          </FormNextButton>
-        </v-stepper-content>
-        <v-stepper-content step="3">
-          <EventFormSummary v-bind="event" />
-          <FormBackButton class="mr-2" @click="step = 2">
-            Back
-          </FormBackButton>
-          <FormNextButton @click="submitEvent">
-            Submit
-          </FormNextButton>
-        </v-stepper-content>
-      </v-stepper-items>
-    </v-stepper>
+        <v-stepper-items class="pb-1">
+          <v-stepper-content step="1">
+            <EventFormContent v-model="valid1" v-bind.sync="content" />
+            <FormNextButton :disabled="!valid1" @click="step = 2">
+              Continue
+            </FormNextButton>
+          </v-stepper-content>
+          <v-stepper-content step="2">
+            <v-checkbox
+              v-model="isPrivate"
+              label="traPが予約していない場所で開催する"
+            />
+            <EventFormReservationPublic
+              v-show="!isPrivate"
+              v-model="validPublic"
+              v-bind.sync="reservationPublic"
+            />
+            <EventFormReservationPrivate
+              v-show="isPrivate"
+              v-model="validPrivate"
+              v-bind.sync="reservationPrivate"
+            />
+            <FormBackButton class="mr-2" @click="step = 1">
+              Back
+            </FormBackButton>
+            <FormNextButton :disabled="!valid2" @click="step = 3">
+              Continue
+            </FormNextButton>
+          </v-stepper-content>
+          <v-stepper-content step="3">
+            <EventFormSummary v-bind="eventSummary" />
+            <FormBackButton class="mr-2" @click="step = 2">
+              Back
+            </FormBackButton>
+            <FormNextButton @click="submitEvent">
+              Submit
+            </FormNextButton>
+          </v-stepper-content>
+        </v-stepper-items>
+      </v-stepper>
 
-    <v-dialog v-model="dialog" width="500">
-      <template #activator="{ on }">
-        <v-card class="px-5 pt-5 pb-3">
-          <span class="headline mr-3">Delete this event</span>
-          <v-btn small depressed color="error" class="mb-2" v-on="on">
-            Delete
-          </v-btn>
+      <v-dialog v-model="dialog" width="500">
+        <template #activator="{ on }">
+          <v-card class="px-5 pt-5 pb-3">
+            <span class="headline mr-3">Delete this event</span>
+            <v-btn small depressed color="error" class="mb-2" v-on="on">
+              Delete
+            </v-btn>
+          </v-card>
+        </template>
+        <v-card>
+          <v-card-title>
+            CAUTION
+          </v-card-title>
+          <v-card-text>
+            This operation cannot be undone. Are you sure?
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn text color="secondary" @click="dialog = false">CANCEL</v-btn>
+            <v-btn text color="error" @click="deleteEvent">DELETE</v-btn>
+          </v-card-actions>
         </v-card>
-      </template>
-      <v-card>
-        <v-card-title>
-          CAUTION
-        </v-card-title>
-        <v-card-text>
-          This operation cannot be undone. Are you sure?
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn text color="secondary" @click="cancelDeletion">CANCEL</v-btn>
-          <v-btn text color="error" @click="deleteEvent">DELETE</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+      </v-dialog>
+    </template>
   </v-container>
 </template>
 
@@ -91,6 +95,14 @@ import EventFormReservationPrivate from '@/components/event/EventFormReservation
 import EventFormSummary from '@/components/event/EventFormSummary.vue'
 import FormNextButton from '@/components/shared/FormNextButton.vue'
 import FormBackButton from '@/components/shared/FormBackButton.vue'
+import ProgressCircular from '@/components/shared/ProgressCircular.vue'
+import LoadFailedText from '@/components/shared/LoadFailedText.vue'
+import { RepositoryFactory } from '@/repositories/RepositoryFactory'
+import moment from 'moment'
+
+const EventsRepo = RepositoryFactory.get('events')
+const RoomsRepo = RepositoryFactory.get('rooms')
+const GroupsRepo = RepositoryFactory.get('groups')
 
 @Component({
   components: {
@@ -100,18 +112,62 @@ import FormBackButton from '@/components/shared/FormBackButton.vue'
     EventFormSummary,
     FormNextButton,
     FormBackButton,
+    ProgressCircular,
+    LoadFailedText,
   },
 })
-export default class New extends Vue {
+export default class EventEdit extends Vue {
+  status: 'loading' | 'loaded' | 'error' = 'loading'
   dialog = false
   step = 1
+
+  event: Schemas.Event = null
+  room: Schemas.Room = null
+  group: Schemas.Group = null
+
+  async created() {
+    await this.fetchEventData()
+    this.assignEventData()
+  }
+
+  async fetchEventData() {
+    const eventId = this.$route.params.id
+    this.status = 'loading'
+    try {
+      this.event = (await EventsRepo.$eventId(eventId).get()).data
+      ;[{ data: this.room }, { data: this.group }] = await Promise.all([
+        RoomsRepo.$roomId(this.event.roomId).get(),
+        GroupsRepo.$groupId(this.event.groupId).get(),
+      ])
+      this.status = 'loaded'
+    } catch (__) {
+      this.status = 'error'
+    }
+  }
+
+  assignEventData() {
+    this.content.name = this.event.name
+    this.content.description = this.event.description
+    this.content.tags = this.event.tags
+    this.content.group = this.group
+    if (this.room.public) {
+      this.reservationPublic.room = this.room
+      this.reservationPublic.timeStart = this.event.timeStart
+      this.reservationPublic.timeEnd = this.event.timeEnd
+      this.reservationPublic.sharedRoom = this.event.sharedRoom
+    } else {
+      this.reservationPrivate.place = this.room.place
+      this.reservationPrivate.timeStart = moment(this.room.timeStart).format()
+      this.reservationPrivate.timeEnd = moment(this.room.timeEnd).format()
+    }
+  }
 
   valid1 = false
   content = {
     name: '',
     tags: [],
     description: '',
-    group: null,
+    group: null as Schemas.Group,
   }
 
   isPrivate = false
@@ -124,20 +180,19 @@ export default class New extends Vue {
     )
   }
   reservationPublic = {
-    room: null,
+    room: null as Schemas.Room,
     timeStart: '',
     timeEnd: '',
     sharedRoom: true,
   }
   reservationPrivate = {
     place: '',
-    date: '',
     timeStart: '',
     timeEnd: '',
   }
 
   // TODO: make more readable
-  get event() {
+  get eventSummary() {
     const reservation = this.isPrivate
       ? this.reservationPrivate
       : this.reservationPublic
@@ -149,22 +204,45 @@ export default class New extends Vue {
       place: this.isPrivate
         ? this.reservationPrivate.place
         : this.reservationPublic.room && this.reservationPublic.room.place,
-      date: this.isPrivate
-        ? this.reservationPrivate.date
-        : this.reservationPublic.room && this.reservationPublic.room.date,
       sharedRoom: this.isPrivate ? null : this.reservationPublic.sharedRoom,
     }
   }
 
-  submitEvent() {}
-
-  deleteEvent() {
-    this.dialog = false
-    alert('delete')
+  async submitEvent() {
+    try {
+      let roomId: string
+      if (this.isPrivate) {
+        roomId = (await RoomsRepo.private.post(this.reservationPrivate)).data
+          .roomId
+      } else {
+        roomId = this.reservationPublic.room.roomId
+      }
+      const reservation = this.isPrivate
+        ? this.reservationPrivate
+        : this.reservationPublic
+      const { eventId } = (await EventsRepo.post({
+        ...this.content,
+        groupId: this.content.group.groupId,
+        roomId,
+        sharedRoom: this.isPrivate ? true : this.reservationPublic.sharedRoom,
+        timeStart: reservation.timeStart,
+        timeEnd: reservation.timeEnd,
+      })).data
+      this.$router.push(`/events/${eventId}`)
+    } catch (__) {
+      alert('Failed to submit...')
+    }
   }
 
-  cancelDeletion() {
-    this.dialog = false
+  async deleteEvent() {
+    const eventId = this.$route.params.id
+    try {
+      await EventsRepo.$eventId(eventId).delete()
+      this.dialog = false
+      this.$router.push('/')
+    } catch (__) {
+      alert('Failed to submit...')
+    }
   }
 }
 </script>
