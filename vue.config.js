41b5a4d3a2692dca3265c3515fb52c6adaaa42f9
wasabi315 <wasabi.nvim.st@gmail.com>
2020-07-13 23:21:22 +0900

    Revert "Change build config"
    This reverts commit a60c2236b4b8770bb8c67e75f0a9aeeaf0f97196.

--- vue.config.js
+++ vue.config.js
@@ -7,31 +7,11 @@ const versionInj = {
   },
 }
 
-const tsconfigFile =
-  process.env.NODE_ENV === 'development'
-    ? 'tsconfig.dev.json'
-    : 'tsconfig.prod.json'
-
 module.exports = {
-  productionSourceMap: false,
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin(versionInj),
       new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /ja/),
     ],
-  },
-  chainWebpack: config => {
-    config.module
-      .rule('ts')
-      .use('ts-loader')
-      .loader('ts-loader')
-      .tap(options => {
-        options.configFile = tsconfigFile
-        return options
-      })
-    config.plugin('fork-ts-checker').tap(args => {
-      args[0].tsconfig = tsconfigFile
-      return args
-    })
   },
 }
