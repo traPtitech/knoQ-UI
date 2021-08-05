23b5e6e7098804e1ed0e39d498ad90cab7efd5bd
wasabi315 <wasabi.nvim.st@gmail.com>
2021-08-05 23:23:38 +0900

    Add scripts

---
+++ scripts/smart-install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+checkForChangeAnd() {
+    local changedFiles="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
+    echo "$changedFiles" | grep --quiet "$1" && eval "$2"
+}
+
+onPackageJsonHasChanged() {
+    echo "Changes to package.json detected, installing updates"
+    yarn install
+}
+
+checkForChangeAnd package.json onPackageJsonHasChanged
