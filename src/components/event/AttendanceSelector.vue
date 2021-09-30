b38951b24109abaa833ee3e7a190b02b7fa671e8
wasabi315 <wasabi.nvim.st@gmail.com>
2021-09-30 15:32:16 +0900

    Add component AttendanceSelector

---
+++ src/components/event/AttendanceSelector.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-select
+    v-model="attendance"
+    :items="items"
+    :value="attendance"
+    solo
+    flat
+    :background-color="colorOfAttendance(attendance)"
+    color="white"
+  >
+    <template #selection="{ item }">
+      <span class="white--text font-weight-medium">{{ item.text }}</span>
+    </template>
+    <template #item="{ item, on, attrs }">
+      <v-list-item
+        v-bind="attrs"
+        :color="colorOfAttendance(item.value)"
+        v-on="on"
+      >
+        {{ item.text }}
+      </v-list-item>
+    </template>
+  </v-select>
+</template>
+
+<script lang="ts">
+import { RequestScheduleScheduleEnum } from '@/api'
+import Vue from 'vue'
+import { Component, ModelSync } from 'vue-property-decorator'
+
+@Component
+export default class AttendanceSelector extends Vue {
+  @ModelSync('value', 'update', { type: String, required: true })
+  attendance!: RequestScheduleScheduleEnum
+
+  items: { text: string; value: RequestScheduleScheduleEnum }[] = [
+    {
+      text: '未定',
+      value: RequestScheduleScheduleEnum.Pending,
+    },
+    {
+      text: '欠席予定',
+      value: RequestScheduleScheduleEnum.Absent,
+    },
+    {
+      text: '出席予定',
+      value: RequestScheduleScheduleEnum.Attendance,
+    },
+  ]
+
+  colorOfAttendance(attendance: RequestScheduleScheduleEnum): string {
+    return {
+      [RequestScheduleScheduleEnum.Pending]: 'pending',
+      [RequestScheduleScheduleEnum.Absent]: 'secondary',
+      [RequestScheduleScheduleEnum.Attendance]: 'primary',
+    }[attendance]
+  }
+}
+</script>
