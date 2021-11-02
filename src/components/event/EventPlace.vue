4f5b557b5f83cf8d932bc1200d87d46de6f8d49f
itt828 <iittt828@gmail.com>
2021-11-02 21:21:57 +0900

    fix

--- src/components/event/EventPlace.vue
+++ src/components/event/EventPlace.vue
@@ -1,13 +1,11 @@
 <template>
-  <div>
-    <div>
-      <v-icon v-if="contains('youtube')" class="mr-1">mdi-youtube</v-icon>
-      <v-icon v-if="contains('discord')" class="mr-1">mdi-discord</v-icon>
-      <v-icon v-if="contains('zoom')" class="mr-1">mdi-video</v-icon>
-      <v-icon v-if="contains('qall')" class="mr-1">mdi-phone</v-icon>
-      {{ place }}
-    </div>
-  </div>
+  <span>
+    <v-icon v-if="contains('youtube')" class="mr-1">mdi-youtube</v-icon>
+    <v-icon v-if="contains('discord')" class="mr-1">mdi-discord</v-icon>
+    <v-icon v-if="contains('zoom')" class="mr-1">mdi-video</v-icon>
+    <v-icon v-if="contains('qall')" class="mr-1">mdi-phone</v-icon>
+    {{ place }}
+  </span>
 </template>
 
 <script lang="ts">
