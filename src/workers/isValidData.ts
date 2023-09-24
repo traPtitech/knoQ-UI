8b05c49bdf477a9952bc1520dd5501bf5657865a
mathsuky <yuida2718@gmail.com>
2023-09-24 20:40:39 +0900

    relax_column_countをfalseに

--- src/workers/isValidData.ts
+++ src/workers/isValidData.ts
@@ -1,8 +1,12 @@
 import { parse } from 'csv-parse/browser/esm/sync'
+
 export function isValidData(data: string): boolean {
-  const records = parse(data, {
-    delimiter: ',',
-    relax_column_count: true,
-  })
-  return records.every(record => record.length === 6)
+  try {
+    const records = parse(data, {
+      delimiter: ',',
+    })
+    return records.every(record => record.length === 6)
+  } catch (error) {
+    return false
+  }
 }
